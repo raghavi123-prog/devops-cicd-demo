f47e7e566a3536f6ae961a5e40f944bd9f902f73
SailajaMan <mekalaraghavi@gmail.com>
2026-08-21 18:30:47 +0530

    Add test_app.py and cloudbuild.yaml for GCP CI pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! My CI/CD Pipeline is Working!"
+    return "Hello! My CI/CD Pipeline is Working and  perform CI !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
