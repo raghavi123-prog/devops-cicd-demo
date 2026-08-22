e5caf7f47c9b58a2d6aa8d106e9aa24de57fbea3
SailajaMan <mekalaraghavi@gmail.com>
2026-08-22 14:43:53 +0530

    new changes made in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! My CI/CD Pipeline is Working and  perform CI !"
+    return "Hello! My CI/CD Pipeline is Working and  perform CI and performing ac !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
