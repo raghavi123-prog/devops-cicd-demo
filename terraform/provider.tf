fb81eb2372d5dd5c745624b91dd848e0023bef23
SailajaMan <mekalaraghavi@gmail.com>
2026-09-05 17:53:22 +0530

    made changes in cloudbuild.yam and few cahnges in iam cloud build add new roles

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,21 +6,8 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
   backend "gcs" {
-    bucket = "project-c6caeb96-abbd-4e9c-bfa-tfstate"
+    bucket = "project-c6caeb96-abbd-4e9c-bfa_cloudbuild"
     prefix = "terraform/state"
   }
 }
@@ -28,4 +15,4 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-}}
+}
