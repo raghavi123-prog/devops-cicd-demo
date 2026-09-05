0f71da4e62464dca60438b58ff837ea137c893cd
SailajaMan <mekalaraghavi@gmail.com>
2026-09-05 19:42:54 +0530

    fix: remove external iam binding on cloud run service

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -8,12 +8,3 @@ resource "google_cloud_run_v2_service" "my_run" {
     }
   }
 }
-
-resource "google_cloud_run_v2_service_iam_member" "noauth" {
-  name     = google_cloud_run_v2_service.my_run.name
-  location = google_cloud_run_v2_service.my_run.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-
-  depends_on = [google_cloud_run_v2_service.my_run]
-}
