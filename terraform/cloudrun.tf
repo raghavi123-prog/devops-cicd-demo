f2cef7f0d55f3118c26ecc3fe1c11c61caf283ef
SailajaMan <mekalaraghavi@gmail.com>
2026-09-05 19:20:26 +0530

    made changes in cloud run and  vm.tf

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -14,4 +14,6 @@ resource "google_cloud_run_v2_service_iam_member" "noauth" {
   location = google_cloud_run_v2_service.my_run.location
   role     = "roles/run.invoker"
   member   = "allUsers"
+
+  depends_on = [google_cloud_run_v2_service.my_run]
 }
