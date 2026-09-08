54ca61f5b90135e0f0e12347be64cef840a3c454
SailajaMan <mekalaraghavi@gmail.com>
2026-09-08 16:58:48 +0530

    fix: remove duplicate output blocks from module main.tf files

--- terraform/modules/artifact-registry/main.tf
+++ terraform/modules/artifact-registry/main.tf
@@ -8,7 +8,3 @@ resource "google_artifact_registry_repository" "repo" {
   description   = "Docker repository managed by Terraform"
   format        = "DOCKER"
 }
-
-output "repository_id" {
-  value = google_artifact_registry_repository.repo.id
-}
--- terraform/modules/service-account/main.tf
+++ terraform/modules/service-account/main.tf
@@ -6,7 +6,3 @@ resource "google_service_account" "sa" {
   account_id   = "dev-app-sa-${random_id.sa_suffix.hex}"
   display_name = "Dev Application Service Account"
 }
-
-output "email" {
-  value = google_service_account.sa.email
-}
